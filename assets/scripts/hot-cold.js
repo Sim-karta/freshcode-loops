6c1a026491275922eabd21be2cbb1d342dfa4f5e
Sim-karta <simonenkonikita561@gmail.com>
2026-08-01 21:11:40 +0300

    Add a play button

--- assets/scripts/hot-cold.js
+++ assets/scripts/hot-cold.js
@@ -1,25 +1,31 @@
-const randomNum = Math.floor(Math.random() * 20) + 1;
-let userNum = 0;
+const buttonElement = document.querySelector(".game-btn");
 
-while (randomNum !== userNum) {
-    userNum = prompt("Введіть число від 1 до 20 - ");
-    if (userNum === null) {
-        break;
-    } else {
-        userNum = Number(userNum);
-    }
+const playGame = () => {
+    const randomNum = Math.floor(Math.random() * 20) + 1;
+    let userNum = 0;
 
-    if (userNum !== randomNum) {
-        if (userNum >= randomNum - 3 && userNum <= randomNum + 3) {
-            console.log("Гаряче");
+    while (randomNum !== userNum) {
+        userNum = prompt("Введіть число від 1 до 20 - ");
+        if (userNum === null) {
+            break;
         } else {
-            console.log("Холодно");
+            userNum = Number(userNum);
+        }
+
+        if (userNum !== randomNum) {
+            if (userNum >= randomNum - 3 && userNum <= randomNum + 3) {
+                console.log("Гаряче");
+            } else {
+                console.log("Холодно");
+            }
         }
     }
-}
 
-if (randomNum === userNum) {
-    console.log(`Ви виграли!!! Це було число ${randomNum}`);
-} else {
-    console.log("В наступний раз пощастить");
-}
+    if (randomNum === userNum) {
+        console.log(`Ви виграли!!! Це було число ${randomNum}`);
+    } else {
+        console.log("В наступний раз пощастить");
+    }
+};
+
+buttonElement.addEventListener("click", playGame);
