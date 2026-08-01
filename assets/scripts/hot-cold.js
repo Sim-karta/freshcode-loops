32b7086cc13e6a27621ad14eaaa15b19e5ba178c
Sim-karta <simonenkonikita561@gmail.com>
2026-08-01 21:06:03 +0300

    Make the logic more complicated

--- assets/scripts/hot-cold.js
+++ assets/scripts/hot-cold.js
@@ -2,12 +2,19 @@ const randomNum = Math.floor(Math.random() * 20) + 1;
 let userNum = 0;
 
 while (randomNum !== userNum) {
-    console.log(userNum);
     userNum = prompt("Введіть число від 1 до 20 - ");
     if (userNum === null) {
         break;
     } else {
         userNum = Number(userNum);
+    }
+
+    if (userNum !== randomNum) {
+        if (userNum >= randomNum - 3 && userNum <= randomNum + 3) {
+            console.log("Гаряче");
+        } else {
+            console.log("Холодно");
+        }
     }
 }
 
