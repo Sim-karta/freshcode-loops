e6773714544e6b9becf07c826dfeb88a578efde5
Sim-karta <simonenkonikita561@gmail.com>
2026-08-01 21:26:03 +0300

    Add options to exit the counter

--- assets/scripts/counter.js
+++ assets/scripts/counter.js
@@ -1,10 +1,10 @@
 let sum = 0;
 let userValue = 0;
 
-while (userValue !== "стоп") {
+while (true) {
     userValue = prompt("Введіть число - ");
 
-    if (userValue === null) {
+    if (userValue === null || userValue === "стоп") {
         break;
     } else {
         userValue = Number(userValue);
